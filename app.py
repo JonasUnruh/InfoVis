a09f40a1a90dbb6c4df347b8a7958cac378083c2
JonasUnruh <jonas.unruh@t-online.de>
2024-05-04 13:19:02 +0200

    added data

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import json
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -11,8 +12,16 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 def data():
+    cleaned_df_player_stats = pd.read_csv("./static/data/cleaned_df_player_stats.csv")
+    df_agg_player = pd.read_csv("./static/data/df_agg_player.csv")
+    df_agg_team = pd.read_csv("./static/data/df_agg_team.csv")
+
+    cleaned_json = json.loads(cleaned_df_player_stats.to_json())
+    player_json = json.loads(df_agg_player.to_json())
+    team_json = json.loads(df_agg_team.to_json())
+    
     # replace this with the real data
-    testData = ["hello", "infovis", "2024"]
+    testData = [cleaned_json, player_json, team_json]
 
     # return the index file and the data
     return render_template("index.html", data=json.dumps(testData))
